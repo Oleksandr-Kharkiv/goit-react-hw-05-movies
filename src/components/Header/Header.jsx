ccf39b48b07da31c37682a2f444ced5ca776d2db
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-06-26 22:44:32 +0200

    fix accent nav link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () =>{
     return (
         <nav className={css.HeaderComponent}>
             <div className={css.navbar}>
-                <NavLink to='/' className={css.navLink}>Home</NavLink>
-                <NavLink to='movies' className={css.navLink}>Movies</NavLink>
+                <NavLink to='/' className={({isActive})=> (isActive ? css.active : css.navLink)}>Home</NavLink>
+                <NavLink to='movies' className={({isActive})=> (isActive ? css.active : css.navLink)}>Movies</NavLink>
             </div>
         </nav>
     )
